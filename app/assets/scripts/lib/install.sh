7df45b66d814b6b5b8265d5dc3f02e2b64700ece
Théristes Gomes <theristes@gmail.com>
2019-02-07 16:02:38 -0200

    Changing the language to python

---
+++ app/assets/scripts/lib/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -O https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
+curl -O https://code.jquery.com/jquery-3.3.1.slim.min.js
+curl -O https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js
+curl -O https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
